ab2ba46e2f49615a4efb45b8d5068f28e4b053d8
nithshreya <sshreyaanand@gmail.com>
2020-05-24 17:23:36 +0530

    testing new branch

--- index.js
+++ index.js
@@ -22,8 +22,6 @@ app.listen(3000, () => console.log('Listening to port 3000'));
 
 
 
-
-
 // app.post('/user', (req, res) => {
 //     const { name, age, sex, pin, role, username, password } = req.body;
 //     const newEntry = {
