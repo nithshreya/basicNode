1076a60c4b45c0445dd89f0aadac276342507440
nithshreya <sshreyaanand@gmail.com>
2020-07-06 13:08:32 +0530

    role authorization added

--- app/middlewares/authentication.js
+++ app/middlewares/authentication.js
@@ -1,14 +1,16 @@
 module.exports = (roles) => {
     return (req, res, next) => {
-        // if (!req.requesterDetails) {
-        //     res.status(401).send({ error: 'you are not authorized to make this request' })
-        // }
-        // let role = req.requesterDetails.role
-        // if (roles.includes(role)) {
-        //     next()
-        // } else {
-        //     res.status(401).send({ error: 'you are not authorized to make this request' })
-        // }
-        next()
+        if (!req.requesterDetails) {
+            res.status(401).send({ error: 'you are not authorized to make this request' })
+        }
+        console.log(req.requesterDetails)
+        let role = req.requesterDetails.role
+        if (roles.includes(role)) {
+            next()
+        } else {
+            res.status(401).send({ error: 'you are not authorized to make this request' })
+            console.log('you are not authorized to make this request')
+        }
+        // next()
     }
 }
--- app/middlewares/tokenVerification.js
+++ app/middlewares/tokenVerification.js
@@ -1,12 +1,18 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
+  console.log(req.headers.authorization);
   const token = req.headers.authorization;
+  console.log(req.headers.authorization);
   if (token) {
     console.log("There was a Token");
     jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
       if (!err) {
         req.requesterDetails = decoded;
+        console.log(decoded)
+      }
+      else{
+        console.log(err)
       }
     });
   } else {
